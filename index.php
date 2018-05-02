65cda13c2d413d563da8468ce8bc87a00cf950a8
ereme <eredondomorgado@gmail.com>
2018-05-02 08:31:10 +0200

    Version inicial

---
+++ index.php
@@ -0,0 +1,208 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.4/css/all.css" integrity="sha384-DmABxgPhJN5jlTwituIyzIUk6oqyzf3+XuP7q3VfcWA2unxgim7OSSZKKf0KSsnh" crossorigin="anonymous">        
+    <link rel="stylesheet" href="style.css">
+    <title>Opendesk - Forniture designed for inspiring workplaces</title>
+
+  </head>
+  <body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <a href=""><img src="imagenes/logo.png" alt="OpenDesk"></a>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Furniture <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Workspaces</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Open making</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Blog</a>
+                    </li>
+                </ul>
+                <span class="navbar-text">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#"> <i class="fas fa-shopping-cart"></i> <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Your dashboard</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Join as a maker</a>
+                        </li>
+                    </ul>
+                </span>
+            </div>
+        </nav>
+    </header>
+
+    <div id="uno" class="filaimagen">
+        <div>
+            <h2>Furniture designed for <br>inspiring workplaces</h2>
+            <p></p>
+            <h4>We’re changing the way furniture is <br>made, by connecting customers to local <br>makers</h4>
+            <buttom class="btn btn-success">Shop Furniture<buttom>
+        </div>
+    </div>
+    
+    <div>
+        <div class="card-group">
+            <div class="card">
+            </div>
+            <div class="card">
+            </div>
+            <div class="card">
+            </div>
+        </div>
+    </div>
+
+    <div id="dos" class="filaimagen">
+        <div>
+            <h2>A global platform for local <br>making</h2>
+            <p></p>
+            <h4>Opendesk is an online marketplace that <br>hosts independently designed furniture <br>
+            and connects its customers to local <br>makers around the world. Rather than <br>
+            mass manufacturing and shipping <br>worldwide, we’re building a distributed <br>
+            and ethical supply chain through a global <br> maker network.</h4>
+            
+        </div>
+    </div>
+
+    
+    <div class="text-center gris">
+        <p>HOW IT WORKS</p>
+        <div class="row">
+            <div class="col">
+                <img src="imagenes/mesa.png" alt="Mesa">
+                <h3>Select design</h3> 
+                <p>Browse and add designs to your basket or get in touch for help</p>
+            </div>
+            <div class="col"> 
+                <img src="imagenes/quotes.png" alt="Quotes">
+                <h3>Request quotes</h3> 
+                <p>Get quotes from local makers in your area within two business days</p>
+            </div>
+            <div class="col">
+                <img src="imagenes/brazo.png" alt="Brazo">
+                <h3>Choose your marker</h3>
+                <p>Choose the local maker’s quote that works best for you</p>  
+            </div>
+            <div class="col"> 
+                <img src="imagenes/mesa2.png" alt="Mesa">
+                <h3>Receive your furniture</h3>
+                <p>Get furniture delivered to your door in 2-4 weeks, direct from your maker</p> 
+            </div>
+        </div>
+        <buttom class="btn btn-success">Get started</buttom>
+    </div>
+
+    <div id="mivideo">
+        <div style="padding:56.25% 0 0 0;position:relative;">
+            <iframe src="https://player.vimeo.com/video/215745438" 
+            style="position:absolute;left:0;top:0;width:100%;height:100%;" frameborder="0" allowfullscreen>
+            </iframe>
+        </div>
+        <script src="https://player.vimeo.com/api/player.js"></script>
+    </div>
+
+    <div class="row marcas">
+        <div class="col">
+            <img src="imagenes/logonike.png" alt="Nike">
+        </div>
+        <div class="col">
+            <img src="imagenes/logowework.png" alt="We Work">
+        </div>
+        <div class="col">
+            <img src="imagenes/logogoogle.png" alt="Google">
+        </div>
+        <div class="col">
+            <img src="imagenes/logogreenpeace.png" alt="Greenpeace">
+        </div>
+    </div>
+
+    <div id="tres" class="filaimagen">
+        <div>
+            <h2>Inspiring workplaces for <br>amazing companies making</h2>
+            <p></p>
+            <h4>We spend one third of our lives working, <br>so our work environments matter. These <br>
+            companies bought Opendesk designs <br>from local makers because they’re <br>
+            beautiful, made on-demand and built for <br>collaboration</h4>
+            
+        </div>
+    </div>
+
+    <div class="text-center guardian">
+        <h2>The Guardian </h2>
+        <p>“Opendesk is changing the way furniture is produced and moved, cutting <br>
+        out time-consuming and expensive shipping, showrooms and storage.”</p>
+    </div>
+
+    <div class="row marcas2">
+        <div class="col">
+            <img src="imagenes/fastcompany.png" alt="Fast Company">
+        </div>
+        <div class="col">
+            <img src="imagenes/wired.png" alt="Wired">
+        </div>
+        <div class="col">
+            <img src="imagenes/techcrunch.png" alt="TechCrunch">
+        </div>
+        <div class="col">
+            <img src="imagenes/theguardian.png" alt="TheGuardian">
+        </div>
+        <div class="col">
+            <img src="imagenes/dezeen.png" alt="Dezeen">
+        </div>
+    </div>
+
+    <footer>
+        <div class="row text-center">
+            <img src="imagenes/logo.png" alt="Logo">
+        </div>
+        <div class="row text-center">
+            <ul>
+                <li>Abour</li>
+                <li>Contact</li>
+                <li>FAQ</li> 
+                <li>Our terms</li>
+                <li>Jobs</li>
+            </ul>
+        </div>
+        <div class="row">
+            <a href="https://twitter.com/open_desk" target="_blank"><i class="fab fa-twitter-square fa-2x"></i> </a>
+            <a href="https://www.facebook.com/opendesk.cc" target="_blank"><i class="fab fa-facebook-square fa-2x"></i> </a>
+            <a href="https://www.instagram.com/open_desk/" target="_blank"><i class="fab fa-instagram fa-2x"></i> </a>
+            <a href="https://www.pinterest.es/open_desk/" target="_blank"><i class="fab fa-pinterest-square fa-2x"></i> </a>
+            <a href="https://www.linkedin.com/company/fabbed" target="_blank"><i class="fab fa-linkedin"></i></a>
+            <a href="https://vimeo.com/opendesk" target="_blank"><i class="fab fa-vimeo"></i></a>
+            <a href="https://www.youtube.com/channel/UCLmzGGt5-3Irwa84zJgwgkQ" target="_blank"><i class="fab fa-youtube-square fa-2x"></i> </a>
+            
+        </div>
+        <div class="row">
+            <div class="col">
+                <buttom class="btn btn"> Sing up to our newsletter</buttom>
+            </div>
+            <div class="col">
+                <p>Let us know your thoughts about things you love and things<br> that can be improved on the site:<br><br><p>
+                <p>https://www.opendesk.cc/the-art-of-sharing</p>
+
+            </div>
+        </div>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>        
+</body>
+</html>
